290a198024e1ed2c6c495d1f30a8f93b723c1aac
HarshalPatil-1214 <patilharshal1214@gmail.com>
2025-09-16 10:59:56 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://github.com/HarshalPatil-1214/first-git-demo",
   plugins: [react()],
 })
